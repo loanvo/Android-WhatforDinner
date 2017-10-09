013c2de12bcc65eb30b778d1d8f16f58d2994e53
loanvo <loan.vo@sjsu.edu>
2017-10-08 20:55:39 -0700

    handle long press with box of text display auhtor and app info -add onlonglick on Oncreate method in mainscreen

--- app/src/main/java/cs175/whatsfordinner/MainScreenActivity.java
+++ app/src/main/java/cs175/whatsfordinner/MainScreenActivity.java
@@ -1,7 +1,9 @@
 package cs175.whatsfordinner;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,13 +28,33 @@ public class MainScreenActivity extends AppCompatActivity {
     private List<String> list;
     protected DBHelper dbHelper;
 
-    //Handle long press
 
-
+    private ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        image = (ImageView) findViewById(R.id.imageView);
+        //Handle long press
+        image.setOnLongClickListener(new View.OnLongClickListener() {
+            public boolean onLongClick(View view) {
+                AlertDialog alertDialog = new AlertDialog.Builder(view.getContext()).create();
+                alertDialog.setTitle("");
+                alertDialog.setMessage("Author:   Loan Vo" + "\n" + "Version:   1.0" + "\n"
+                                        + "Link for help:   https://developer.android.com/develop/index.html"
+                                + "\n\n" + "Copyright © Loan Vo, 2017");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+
+                return true;
+            }
+        });
 
     }
 
